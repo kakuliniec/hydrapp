dc8c03a79504436213cc3af5f3d5bf92fa36a358
Kaka K <kakuliniec@wp.pl>
2019-07-29 20:57:23 +0200

    feat: js

--- src/js/main.js
+++ src/js/main.js
@@ -17,6 +17,28 @@ if ('serviceWorker' in navigator) {
 // place your code below
 
 
-console.log(`Hello world!`);
 
+const buttonAdd = document.querySelector('.button-add--js');
+const buttonRemove = document.querySelector('.button-remove--js');
+const value = document.querySelector('.counter__value--js');
+const key =  new Date().toISOString().slice(0, 10)
+
+if (!localStorage.getItem(key)) {
+  localStorage.setItem(key, 0);
+  value.innerHTML = '0';
+} else {
+  value.innerHTML = localStorage.getItem(key);
+}
+
+buttonAdd.addEventListener('click', (e) => {
+  localStorage.setItem(key, parseInt(localStorage.getItem(key)) + 1);
+  value.innerHTML = localStorage.getItem(key);
+})
+ 
+buttonRemove.addEventListener('click', (e) => {
+  const currentValue = parseInt(localStorage.getItem(key));
+  if (currentValue > 0) {
+    localStorage.setItem(key, localStorage.getItem(key) - 1);
+    value.innerHTML = localStorage.getItem(key);}
+})
 
